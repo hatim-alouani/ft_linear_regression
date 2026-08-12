72eb877e2c5d84850f92ad4912479d69dd96f7ec
Hatim Alouani <alouanihatim01@gmail.com>
2026-08-12 14:46:41 +0100

    push

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ df = pd.read_csv("data.csv")
 mileage = df["km"].values
 price = df["price"].values
 
+m = len(price)
+
 mileage_min = mileage.min()
 mileage_max = mileage.max()
 
@@ -19,17 +21,16 @@ iterations = 10000
 
 # math formulat
 # estimated_price = theta1 * mileage + theta0
-# J = 1/2n * ​∑(estimated_price[i] − price[i])^2
-# ∂J/∂theta1 = 1/n * ∑mileage * (estimated_price[i] − price[i])
-# ∂J/∂theta0 = 1/n * ∑(estimated_price[i] − price[i])
+# J = 1/2m * ​∑(estimated_price[i] − price[i])^2
+# theta = theta - learning_rate * ∂J/∂theta
+# ∂J/∂theta0 = 1/m * ∑(estimated_price[i] − price[i])
+# ∂J/∂theta1 = 1/m * ∑mileage * (estimated_price[i] − price[i])
 # tmp_theta1 = learning_rate * ∂J/∂theta1
 # tmp_theta0 = learning_rate * ∂J/∂theta0
 # theta1 = theta1 - tmp_theta1
 # theta0 = theta0 - tmp_theta0
 
 def gradient_descent(mileage, price, theta0, theta1, learning_rate, iterations):
-
-    m = len(price)
 
     for _ in range(iterations):
 
@@ -52,13 +53,8 @@ theta1 = theta1_normalized / (mileage_max - mileage_min)
 
 theta0 = theta0_normalized - (theta1_normalized * mileage_min / (mileage_max - mileage_min))
 
-model = {
-    "theta0": theta0,
-    "theta1": theta1
-}
-
 with open("theta.json", "w") as file:
-    json.dump(model, file)
+    json.dump({ "theta0": theta0, "theta1": theta1 }, file)
 
 print("Training completed.")
 print(f"theta0: {theta0}")
